22e3efb01d5b2ad3f0b6cc1da96aad77dd4dba1d
Damian Hernandez <dhernandezmerino@outlook.com>
2019-11-13 10:43:04 -0600

    subir archivos

---
+++ archivo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'config.inc.php';
+        $db = new Conect_MySql();
+        $sql = "select*from tbl_documentos where id_documento=" . $_GET['id'];
+        $query = $db->execute($sql);
+        if ($datos = $db->fetch_row($query)) {
+            if ($datos['nombre_archivo'] == "") {
+                ?>
+                <p>NO tiene archivos</p>
+                <?php
+            } else {
+                header("Content-type: application/pdf ");
+                ?>
+                <?php echo $datos['pdf']; ?>"></iframe>
+
+        <?php }
+    }
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+include_once 'config.inc.php';
+if (isset($_POST['subir'])) {
+    $nombre = $_FILES['archivo']['name'];
+    $tipo = $_FILES['archivo']['type'];
+    $tamanio = $_FILES['archivo']['size'];
+    $ruta = $_FILES['archivo']['tmp_name'];
+    $destino = $nombre;
+    $image = $_FILES['archivo']['tmp_name'];
+    $imgContent = addslashes(file_get_contents($image));
+    if ($nombre != "") {
+        $titulo = $_POST['titulo'];
+        $descri = $_POST['descripcion'];
+        $db = new Conect_MySql();
+        $sql = "INSERT INTO tbl_documentos(titulo,descripcion,tamanio,tipo,nombre_archivo,pdf) VALUES('$titulo','$descri','$tamanio','$tipo','$nombre','$imgContent')";
+        $query = $db->execute($sql);
+        if ($query) {
+            echo "Se guardo correctamente";
+        }
+    } else {
+        echo "Error";
+    }
+}
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <div style="width: 500px;margin: auto;border: 1px solid blue;padding: 30px;">
+            <h4>Subir PDF</h4>
+            <form method="post" action="" enctype="multipart/form-data">
+                <table>
+                    <tr>
+                        <td><label>Titulo</label></td>
+                        <td><input type="text" name="titulo"></td>
+                    </tr>
+                    <tr>
+                        <td><label>Descripcion</label></td>
+                        <td><textarea name="descripcion"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="file" name="archivo"></td>
+                    <tr>
+                        <td><input type="submit" value="subir" name="subir"></td>
+                        <td><a href="lista.php">lista</a></td>
+                    </tr>
+                </table>
+        </div>
+    </body>
+</html>
